af394fd86e49aca7f8b2e1e9e568e7272503814e
Manikandan Prabhu <manikandanrprabhu@gmail.com>
2022-01-20 18:46:05 +0530

    V1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,44 @@ window.addEventListener('load', () =>{
     preload.style.display= "none";
 }  
 )
+
+
+var prevScrollpos = window.pageYOffset;
+console.log(prevScrollpos)
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var navbar = document.querySelector(".navbar");
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+const link_button = document.querySelector(".intro-btn");
+link_button.addEventListener('click', changeGrid);
+
+function changeGrid(event){
+  event.preventDefault();
+  document.querySelector(".content-home-2").style.display = "none";
+  document.querySelector(".content-home-3").style.display = 'none';
+  document.querySelector(".content-home-1").style.display = 'grid';
+}
+
+const link_button2 = document.querySelector(".work-btn");
+link_button2.addEventListener('click', changeGrid2);
+
+function changeGrid2(event){
+  event.preventDefault();
+    document.querySelector(".content-home-1").style.display = 'none';
+    document.querySelector(".content-home-3").style.display = 'none';
+    document.querySelector(".content-home-2").style.display = "grid";
+}
+
+const link_button3 = document.querySelector(".story-btn");
+link_button3.addEventListener('click', () => {
+    document.querySelector(".content-home-1").style.display = 'none';
+    document.querySelector(".content-home-2").style.display = "none";
+    document.querySelector(".content-home-3").style.display = "grid";
+});
--- assets/js/faqs.js
+++ assets/js/faqs.js
@@ -3,49 +3,41 @@ const btn2 = document.querySelector(".ques-2");
 const btn3 = document.querySelector(".ques-3");
 const btn4 = document.querySelector(".ques-4");
 const btn5 = document.querySelector(".ques-5");
-console.log("hello");
+
 btn1.addEventListener('click',view1);
 btn2.addEventListener('click',view2);
 btn3.addEventListener('click',view3);
 btn4.addEventListener('click',view4);
 btn5.addEventListener('click',view5);
 
+const ans1 = document.querySelector(".ans-1");
+const ans2 = document.querySelector(".ans-2");
+const ans3 = document.querySelector(".ans-3");
+const ans4 = document.querySelector(".ans-4");
+const ans5 = document.querySelector(".ans-5");
+
 function view1(event) {
-    const ans1 = document.querySelector(".ans-1");
-    const ans2 = document.querySelector(".ans-2");
-    const ans3 = document.querySelector(".ans-3");
-    const btn1 = document.querySelector(".ques-1");
     ans1.classList.toggle("hide-answers");
     btn1.classList.toggle("highlight");
     ans2.classList.add("hide-answers");
     ans3.classList.add("hide-answers");
 }
 function view2(event) {
-    const ans1 = document.querySelector(".ans-1");
-    const ans2 = document.querySelector(".ans-2");
-    const ans3 = document.querySelector(".ans-3");
     ans1.classList.add("hide-answers");
     ans2.classList.toggle("hide-answers");
     ans3.classList.add("hide-answers");
 }
 function view3(event) {
-    const ans1 = document.querySelector(".ans-1");
-    const ans2 = document.querySelector(".ans-2");
-    const ans3 = document.querySelector(".ans-3");
     ans1.classList.add("hide-answers");
     ans2.classList.add("hide-answers");
     ans3.classList.toggle("hide-answers");
 }
 
 function view4(event) {
-    const ans4 = document.querySelector(".ans-4");
-    const ans5 = document.querySelector(".ans-5");
     ans4.classList.toggle("hide-answers");
     ans5.classList.add("hide-answers");
 }
 function view5(event) {
-    const ans4 = document.querySelector(".ans-4");
-    const ans5 = document.querySelector(".ans-5");
     ans4.classList.add("hide-answers");
     ans5.classList.toggle("hide-answers");
 }
